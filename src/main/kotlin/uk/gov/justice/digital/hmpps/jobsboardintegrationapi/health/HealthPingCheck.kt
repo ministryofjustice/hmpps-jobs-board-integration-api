2e96c2bdceae0b9d3cc8525b3d57a005fade46a0
jgomezzmoj <168651803+jgomezzmoj@users.noreply.github.com>
2024-12-03 18:07:02 +0000

    updating project name after deployment from template repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/health/HealthPingCheck.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/health/HealthPingCheck.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.templatepackagename.health
+package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.health
 
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
