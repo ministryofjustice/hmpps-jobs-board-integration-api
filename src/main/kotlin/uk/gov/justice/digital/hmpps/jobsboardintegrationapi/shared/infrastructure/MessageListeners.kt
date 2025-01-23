a11c8c876736e1aeb14d9a05615bde7a19e56ddc
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-01-23 21:45:50 +0000

    [ESWE-1181] Fix MessageListeners issue at payload and header/messageAttributes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/MessageListeners.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/MessageListeners.kt
@@ -1,39 +1,54 @@
 package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.slf4j.LoggerFactory
+import org.springframework.messaging.MessageHeaders
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.Headers
+import org.springframework.messaging.handler.annotation.MessageMapping
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.IntegrationEvent
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.IntegrationMessageService
 
 const val ATTRIBUTE_EVENT_TYPE = "eventType"
+const val ATTRIBUTE_EVENT_ID = "eventId"
+const val ATTRIBUTE_ID = "id"
 
 class IntegrationMessageListener(
   private val integrationMessageService: IntegrationMessageService,
-  private val objectMapper: ObjectMapper,
 ) {
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 
+  @MessageMapping
   @SqsListener("integrationqueue", factory = "hmppsQueueContainerFactoryProxy")
-  fun processMessage(message: Message) {
-    val event = objectMapper.readValue(message.message, IntegrationEvent::class.java)
-    integrationMessageService.handleMessage(event, message.messageAttributes)
+  fun processMessage(
+    message: String,
+    @Header(ATTRIBUTE_EVENT_TYPE) eventType: String,
+    @Header(ATTRIBUTE_ID) messageId: String,
+    @Headers headers: MessageHeaders,
+  ) {
+    val messageAttributes = MessageAttributes(headers)
+    val eventId = messageAttributes.eventId
+    log.info("processMessage()|Processing message:messageId=$messageId, eventType=$eventType, eventId=$eventId")
+    val event = IntegrationEvent(
+      eventType = eventType,
+      message = message,
+    )
+    integrationMessageService.handleMessage(event, messageAttributes)
   }
 }
 
-data class MessageAttribute(val value: String, val type: String) {
-  constructor(value: String) : this(value, "String")
-}
-typealias EventType = MessageAttribute
-
-class MessageAttributes() : HashMap<String, MessageAttribute>() {
-  constructor(attribute: EventType) : this() {
-    put(ATTRIBUTE_EVENT_TYPE, attribute)
+class MessageAttributes() : HashMap<String, Any?>() {
+  constructor(eventType: String) : this() {
+    put(ATTRIBUTE_EVENT_TYPE, eventType)
   }
 
-  val eventType: String? get() = this[ATTRIBUTE_EVENT_TYPE]?.value
-}
+  constructor(source: Map<String, Any?>) : this() {
+    putAll(source)
+  }
 
-data class Message(
-  val message: String,
-  val messageId: String,
-  val messageAttributes: MessageAttributes,
-)
+  val eventType: String? get() = get(ATTRIBUTE_EVENT_TYPE) as? String?
+  val eventId: String? get() = get(ATTRIBUTE_EVENT_ID) as? String?
+  val messageId: String? get() = get(ATTRIBUTE_ID) as? String?
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/employers/application/EmployerMessageServiceTestCase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/employers/application/EmployerMessageServiceTestCase.kt
@@ -4,7 +4,6 @@ import org.mockito.Mock
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.employers.domain.EmployerEvent
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.application.UnitTestBase
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.IntegrationEvent
-import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.EventType
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.MessageAttributes
 
 abstract class EmployerMessageServiceTestCase : UnitTestBase() {
@@ -14,14 +13,11 @@ abstract class EmployerMessageServiceTestCase : UnitTestBase() {
   @Mock
   protected lateinit var employerRegistrar: EmployerRegistrar
 
-  protected fun EmployerEvent.messageAttributes() = MessageAttributes(this.eventTypeAsAttribute())
-
-  protected fun EmployerEvent.eventTypeAsAttribute() = EventType(this.eventType.type)
+  protected fun EmployerEvent.messageAttributes() = MessageAttributes(this.eventType.type)
 
   protected fun EmployerEvent.toIntegrationEvent() = IntegrationEvent(
     eventId = this.eventId,
     eventType = this.eventType.type,
-    timestamp = this.timestamp,
-    content = objectMapper.writeValueAsString(this),
+    message = objectMapper.writeValueAsString(this),
   )
 }
