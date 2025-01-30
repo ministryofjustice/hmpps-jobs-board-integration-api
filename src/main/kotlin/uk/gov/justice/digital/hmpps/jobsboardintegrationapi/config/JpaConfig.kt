8c256ffbd70cf5362c242b7a3e5202b043a60d75
Rick Choi <rick.choi@digital.justice.gov.uk>
2025-01-30 16:54:35 +0000

    [ESWE-1181] Fix API clients, JPA auditing (#25)
    Fix API clients
    1) MN API client: missing body
    2) JB API client: scopes' separator
    3) Error & debug logging
    4) Rename container name of local `hmpps-auth` (avoid conflict at
    Docker)
    
    Fix JPA auditing
    - enable JPA auditing for `@CreatedDate` and `@LastModifiedDate`

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/MNJobBoardApiWebClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/MNJobBoardApiWebClient.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.config.ConditionalOnIntegrationEnabled
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.MNJobBoardApiClient
@@ -23,9 +24,15 @@ class MNJobBoardApiWebClient(
 
   override fun createEmployer(request: CreatEmployerRequest): CreatEmployerResponse {
     log.debug("Creating employer employerName={}", request.employerName)
-    return mnJobBoardWebClient.post().uri(EMPLOYERS_ENDPOINT).accept(APPLICATION_JSON).retrieve()
+    log.trace("Create employer request={}", request)
+    return mnJobBoardWebClient.post().uri(EMPLOYERS_ENDPOINT)
+      .accept(APPLICATION_JSON).body(Mono.just(request), request.javaClass)
+      .retrieve()
       .bodyToMono(MNCreatEmployerResponse::class.java)
-      .onErrorResume { error -> Mono.error(Exception("Fail to create employer", error)) }.block()!!
+      .onErrorResume { error ->
+        val errorResponse = if (error is WebClientResponseException) error.responseBodyAsString else null
+        Mono.error(Exception("Fail to create employer! errorResponse=$errorResponse", error))
+      }.block()!!
       .responseObject
   }
 }
