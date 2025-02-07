44fdcd76def148c948858e762adceff741059fad
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-02-07 12:34:37 +0000

    [ESWE-1183] JB API client

--- src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/JobsBoardApiWebClientShould.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/infrastructure/JobsBoardApiWebClientShould.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.employers.domain.EmployerObjects.sainsburys
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobObjects.tescoWarehouseHandler
 
 @ExtendWith(MockitoExtension::class)
 class JobsBoardApiWebClientShould : JobsBoardApiWebClientTestBase() {
@@ -26,5 +27,23 @@ class JobsBoardApiWebClientShould : JobsBoardApiWebClientTestBase() {
 
     private fun replyOnGetEmployerById(response: GetEmployerResponse, employerId: String) =
       replyOnRequestById(response.javaClass, response, uri, employerId)
+  }
+
+  @Nested
+  inner class GivenAnEmployerWithAJob {
+    private val job = tescoWarehouseHandler.copy(createdAt = defaultCurrentTime)
+    private val uri = "/jobs/{id}"
+
+    @Test
+    fun `return job details with a valid job ID`() {
+      replyOnGetJobById(GetJobResponse.from(job), job.id)
+
+      val actualJob = jobsBoardApiWebClient.getJob(job.id)
+
+      assertThat(actualJob).isEqualTo(job)
+    }
+
+    private fun replyOnGetJobById(response: GetJobResponse, jobId: String) =
+      replyOnRequestById(response.javaClass, response, uri, jobId)
   }
 }
