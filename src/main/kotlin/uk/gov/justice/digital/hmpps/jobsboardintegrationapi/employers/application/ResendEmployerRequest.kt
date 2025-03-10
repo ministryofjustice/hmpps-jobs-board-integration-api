e48a06cb7aabae7ca0fa161e3f852f5fc84da116
Rick Choi <rick.choi@digital.justice.gov.uk>
2025-03-10 09:51:58 +0000

    [ESWE-1249] Revise APi docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/employers/application/ResendEmployerRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/employers/application/ResendEmployerRequest.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.employers.applicati
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class ResendEmployerRequest(
-  @Schema(description = "The identifier of the employers (optional)")
+  @Schema(description = "The identifiers of the employer (optional)")
   val employerIds: List<String>? = null,
   @Schema(description = "force updating the given employers (explicit employer IDs are required); No effect without the employer IDs; false by default")
   val forceUpdate: Boolean = false,
