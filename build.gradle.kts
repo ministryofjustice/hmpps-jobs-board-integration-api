0229c3f00adc43ac37e0f8af3b3be24005d2bcef
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-03-11 14:24:08 +0000

    [ESWE-1258] Upgrade dependencies; Add suppression
    - upgrade gradle-spring-boot plugin (`7.1.3`, with Spring Boot to `3.4.3`)
    - suppress for `wiremock-standalone`
    - upgrade sqs lib to `5.3.2`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.3"
   kotlin("plugin.spring") version "2.1.10"
   `jvm-test-suite`
 }
@@ -8,15 +8,12 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-ext["logback.version"] = "1.5.15"
-ext["json-smart.version"] = "2.5.2"
-
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.2.1")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.2")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
@@ -41,7 +38,7 @@ testing {
       dependencies {
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
         implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.2.1")
-        implementation("org.wiremock:wiremock-standalone:3.11.0")
+        implementation("org.wiremock:wiremock-standalone:3.12.1")
         implementation("io.swagger.parser.v3:swagger-parser:2.1.25") {
           exclude(group = "io.swagger.core.v3")
           exclude(group = "io.swagger.parser.v3", module = "swagger-parser-safe-url-resolver")
@@ -75,4 +72,8 @@ tasks {
   check {
     dependsOn(named("test"), named("integrationTest"))
   }
+}
+
+dependencyCheck {
+  suppressionFiles.add("jobs-board-integration-suppressions.xml")
 }
