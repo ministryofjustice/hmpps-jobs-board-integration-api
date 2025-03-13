eea6cdaa7d7fd677b1c6733e40cb795b0940448c
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-03-13 16:10:47 +0000

    [ESWE-1250] Resend Jobs; job services

--- src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/jobs/application/JobServices.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/jobs/application/JobServices.kt
@@ -1,10 +1,18 @@
 package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.application
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.config.ConditionalOnIntegrationEnabled
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.employers.domain.EmployerExternalIdRepository
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.Job
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobEvent
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobEventType
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobEventType.JOB_CREATED
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobEventType.JOB_UPDATED
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobExternalId
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.jobs.domain.JobExternalIdRepository
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.refdata.domain.RefData
@@ -17,12 +25,19 @@ import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.refdata.domain.RefDa
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.refdata.domain.RefData.SALARY_PERIOD
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.refdata.domain.RefData.WORK_PATTERN
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.refdata.domain.RefDataMappingRepository
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.application.UUIDGenerator
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.EventData
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.EventEmitter
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.JobsBoardApiClient
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.MNJobBoardApiClient
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.domain.TimeProvider
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.CreateJobRequest
+import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.GetJobsResponse
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.MNExcludingOffences
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.MNJob
 import uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.infrastructure.UpdateJobRequest
+
+const val FETCH_SIZE = 50
 
 @ConditionalOnIntegrationEnabled
 @Service
@@ -37,10 +52,14 @@ class JobRetriever(
 @ConditionalOnIntegrationEnabled
 @Service
 class JobRegistrar(
+  private val jobsBoardApiClient: JobsBoardApiClient,
   private val mnJobBoardApiClient: MNJobBoardApiClient,
   private val jobExternalIdRepository: JobExternalIdRepository,
   private val employerExternalIdRepository: EmployerExternalIdRepository,
   private val refDataMappingRepository: RefDataMappingRepository,
+  private val eventEmitter: EventEmitter,
+  private val uuidGenerator: UUIDGenerator,
+  private val timeProvider: TimeProvider,
 ) {
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
@@ -75,6 +94,44 @@ class JobRegistrar(
       }
     }
   }
+
+  /**
+   * @return sentCount, totalCount
+   */
+  fun discoverAndResend(fetchSize: Int = FETCH_SIZE): Pair<Long, Long> {
+    log.info("Resending jobs|started")
+    var totalSentCount = 0L
+    var currentPage: GetJobsResponse? = null
+    do {
+      log.info("Resending jobs|discovering")
+      val pageNumber = currentPage?.let { it.page.number + 1 } ?: 0
+      currentPage = retrieveAll(pageNumber, fetchSize)
+      log.info("Resending jobs|sending page {}", currentPage.page.number)
+      totalSentCount += doResend(currentPage.content.map { it.id })
+    } while (currentPage!!.hasNext())
+
+    log.info("Resending jobs|completed: itemCount={}, totalCount={}", totalSentCount, 0)
+    return Pair(totalSentCount, currentPage.page.totalElements)
+  }
+
+  /**
+   * @return sentCount
+   */
+  fun resend(jobIds: List<String>, forceUpdate: Boolean = false, fetchSize: Int = FETCH_SIZE): Int {
+    log.info("Resending jobs|started")
+    var totalSentCount = 0
+
+    jobIds.chunked(fetchSize).forEachIndexed { index, chunk ->
+      log.info("Resending jobs|sending chunk {}", index)
+      log.info("Resending jobs|chunk = {}", chunk)
+      totalSentCount += doResend(chunk, forceUpdate)
+    }
+
+    log.info("Resending jobs|completed: itemCount={}, totalCount={}", totalSentCount, 0)
+    return totalSentCount
+  }
+
+  private fun retrieveAll(page: Int, pageSize: Int): GetJobsResponse = jobsBoardApiClient.getAllJobs(page, pageSize)
 
   private fun create(mnJob: MNJob): MNJob {
     val request = CreateJobRequest.from(mnJob)
@@ -153,4 +210,79 @@ class JobRegistrar(
   private fun translateId(refData: RefData, value: String) = refDataMappingRepository.findByDataRefDataIgnoreCaseAndDataValueIgnoreCase(refData.type, value)?.data?.externalId ?: run {
     throw IllegalArgumentException("Reference data does not exist! refData=${refData.type}: value=$value")
   }
+
+  private fun doResend(ids: List<String>, forceUpdate: Boolean = false): Int = when {
+    ids.isEmpty() -> 0
+    !forceUpdate -> ids.filter { id -> !existsIdMappingById(id) }.toList().let { sendEvents(JOB_CREATED, it) }
+    else -> ids.groupBy { id -> !existsIdMappingById(id) }.filterValues { it.isNotEmpty() }.map {
+      val type = if (it.key) JOB_CREATED else JOB_UPDATED
+      sendEvents(type, it.value)
+    }.sum()
+  }
+
+  private fun sendEvents(eventType: JobEventType, ids: List<String>): Int {
+    var totalSentCount = 0
+    if (ids.isNotEmpty()) {
+      log.debug("Resending jobs|sending with these IDs: {}", ids)
+      ids.map { id -> makeEventForJob(id, eventType) }.toTypedArray().let { events ->
+        log.debug("Sending events {}", events)
+        sendEvent(*events).let { sentCount -> totalSentCount += sentCount }
+      }
+    }
+    return totalSentCount
+  }
+
+  private fun makeEventForJob(
+    jobId: String,
+    jobEventType: JobEventType,
+  ) = JobEvent(
+    eventId = uuidGenerator.generate(),
+    eventType = jobEventType,
+    timestamp = timeProvider.nowAsInstant(),
+    jobId = jobId,
+  )
+
+  private fun sendEvent(vararg jobEvents: JobEvent): Int = runBlocking {
+    val sendTasks = jobEvents.map {
+      async(Dispatchers.IO) {
+        try {
+          eventEmitter.send(it.eventData())
+          sendEventSuccess(it.jobId)
+        } catch (ex: Throwable) {
+          sendEventFailure(ex)
+        }
+      }
+    }
+
+    val results = sendTasks.awaitAll().toList()
+    val completedIds = results.mapNotNull { it.first }.toList()
+    val errors = results.mapNotNull { it.second }.toList()
+    if (errors.isNotEmpty()) {
+      errors.groupingBy { it.message }.eachCount()
+        .map { "Error: ${it.key}, count: ${it.value}" }.joinToString(separator = ";")
+        .let { log.error("Error sending job events (thus skipped), total error count: {}, errors: {}", errors.size, it) }
+    }
+    log.trace("Completed job events with these job IDs: {}", completedIds)
+
+    completedIds.size
+  }
+
+  private fun JobEvent.eventData() = EventData(
+    eventId = eventId,
+    eventType = eventType.type,
+    timestamp = timestamp,
+    content = """
+       {
+      "eventId": "$eventId",
+      "eventType": "${eventType.eventTypeCode}",
+      "timestamp": "$timestamp",
+      "jobId": "$jobId"
+      }
+    """.trimIndent(),
+  )
 }
+
+private fun sendEventSuccess(id: String) = SendEventResult(id, null)
+private fun sendEventFailure(error: Throwable) = SendEventResult(null, error)
+
+private typealias SendEventResult = Pair<String?, Throwable?>
