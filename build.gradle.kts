68f6e17daef1d14c76bdd37581a9e82c45cc9970
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-06-23 20:07:59 +0100

    [ESWE-1345] Upgrade dependencies
    Upgrade:
    * `gradle-spring-boot` `8.3.0` (was `8.3.0-beta`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
   kotlin("plugin.spring") version "2.1.21"
   `jvm-test-suite`
 }
