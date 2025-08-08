7245badf7aa246c77607f48194df4ae252ddc36a
Allie Kane <allison.kane@justice.gov.uk>
2025-08-08 12:16:44 +0100

    update dependancies (#75)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.9")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
@@ -37,9 +37,9 @@ testing {
     register<JvmTestSuite>("integrationTest") {
       dependencies {
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
-        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.10")
-        implementation("org.wiremock:wiremock-standalone:3.13.0")
-        implementation("io.swagger.parser.v3:swagger-parser-v3:2.1.28") {
+        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.11")
+        implementation("org.wiremock:wiremock-standalone:3.13.1")
+        implementation("io.swagger.parser.v3:swagger-parser-v3:2.1.31") {
           exclude(group = "io.swagger.core.v3")
           exclude(group = "io.swagger.parser.v3", module = "swagger-parser-v2-converter")
           exclude(group = "io.swagger.parser.v3", module = "swagger-parser-safe-url-resolver")
