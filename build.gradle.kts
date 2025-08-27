015f48120b31bc7954b2cb60df22d8fe0eda9a3a
Rick Choi <rick.choi@justice.gov.uk>
2025-08-27 17:22:15 +0100

    [ESWE-1414] Upgrade dependencies for OWASP scan's fix (#77)
    Upgrade:
    * Spring Boot plugin `9.0.0` (was `8.3.7`)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
   kotlin("plugin.spring") version "2.2.10"
-  id("org.owasp.dependencycheck") version "12.1.3"
   `jvm-test-suite`
 }
 
