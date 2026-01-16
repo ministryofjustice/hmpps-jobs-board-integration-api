2c9648ea14bf32d14223e1d4297a7803b91deb3b
rickchoijd <rick.choi@justice.gov.uk>
2026-01-16 17:29:36 +0000

    ESWE-1651: Failed test for status-check

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/jobsboardintegrationapi/shared/application/DummyTest.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.jobsboardintegrationapi.shared.application
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+class DummyTest {
+  @Test
+  fun `should fail this test`(): Unit = fail("Fail this test for status-check")
+}
