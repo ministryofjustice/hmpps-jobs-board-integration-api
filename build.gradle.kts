b28b4dc77b67f3fb0a3e278ece3a3dea8defe921
rickchoijd <rick.choi@justice.gov.uk>
2026-02-25 17:29:56 +0000

    ESWE-1721: Update dependencies
    plugins
    - `uk.gov.justice.hmpps.gradle-spring-boot` to `10.0.4`
    - Kotlin to `2.3.10`
    
    dependencies
    - HMPPS Kotlin lib to `2.0.1`
    - HMPPS SQS lib to `7.0.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.3"
-  kotlin("plugin.spring") version "2.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.4"
+  kotlin("plugin.spring") version "2.3.10"
   `jvm-test-suite`
 }
 
@@ -9,8 +9,8 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.0.1")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -39,7 +39,7 @@ testing {
     register<JvmTestSuite>("integrationTest") {
       dependencies {
         kotlin.target.compilations { named("integrationTest") { associateWith(getByName("main")) } }
-        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.0")
+        implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.1")
         implementation("org.wiremock:wiremock-standalone:3.13.2")
         implementation("io.swagger.parser.v3:swagger-parser-v3:2.1.37") {
           exclude(group = "io.swagger.core.v3")
